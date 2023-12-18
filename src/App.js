52462994b504f0536fcf88ab654b218820ac4639
BeWolfff <93709392+BeWolfff@users.noreply.github.com>
2023-12-18 12:32:37 +0100

    rollback

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
-// TodoListApp.js
-import React from 'react';
-import { View } from 'react-native';
-import TodoList from './TodoList';
+import logo from './logo.svg';
+import './App.css';
 
-const TodoListApp = () => {
+function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TodoList />
-    </View>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload and BUILD with Bereniiiiiice.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React and BUILD with Berenice today !!
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
-export default TodoListApp;
-
-// TodoList.js
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import axios from 'axios';
-import TodoItem from './TodoItem';
-
-const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    axios.get('ton_url_api_aws/todos')
-      .then(response => {
-        setTodos(response.data);
-      })
-      .catch(error => {
-        console.error('Erreur de chargement des tâches : ', error);
-      });
-  }, []);
-
-  return (
-    <View>
-      <FlatList
-        data={todos}
-        renderItem={({ item }) => <TodoItem todo={item} />}
-        keyExtractor={item => item.id.toString()}
-      />
-    </View>
-  );
-};
-
-export default TodoList;
-
-// TodoItem.js
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const TodoItem = ({ todo }) => {
-  return (
-    <View style={styles.todoItem}>
-      <Text>{todo.task}</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  todoItem: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-});
-
-export default TodoItem;
+export default App;
