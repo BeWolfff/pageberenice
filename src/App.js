268b6d7d9a6dba186838d090f087737f3a9dfca9
BeWolfff <93709392+BeWolfff@users.noreply.github.com>
2023-12-18 14:58:03 +0100

    1ere correction

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and BUILD with Bereniiiiiice.
+          Edit <code>src/App.js and save to reload and BUILD with Bereniiiiiice.</code> 
         </p>
         <a
           className="App-link"
