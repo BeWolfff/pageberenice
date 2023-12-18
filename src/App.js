9f4df890d80bca9211c76f9d7752a5a7688cad6d
BeWolfff <93709392+BeWolfff@users.noreply.github.com>
2023-12-18 17:19:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js andAAAAAAAAAAAAAAAAAAAAAAAAA
+          Edit src/App.js andAAAAAAAAAAAAAAAAAAAAAAAAA
         <a
           className="App-link"
           href="https://reactjs.org"
